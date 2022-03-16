24447ed0c8c26bed629b640fa1ea1293a32771d7
Anibal António <djani454@hotmail.com>
2022-03-16 11:07:57 +0100

    feat(Home): :lipstick: Add transactions form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,16 +4,31 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from './'
 
+const Input = ({placeholder, name, type, value, handleChange}) => (
+    <input 
+        placeholder={placeholder}
+        name={name}
+        type={type}
+        valeu={value}
+        onChange={(e) => handleChange(e, name)}
+        step="0.00001"
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism" 
+
+    />
+)
+
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Welcome = () => {
     const connectWallet = () => {
 
     }
+
+    const handleSubmit = () => {}
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br /> across the world
                     </h1>
@@ -22,7 +37,7 @@ const Welcome = () => {
                     </p>
                     <button 
                         type="button" 
-                        onClick="connectWallet"
+                        onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
                     >
                         <p className="text-white text-base font-semibold">ConnectWallet</p>
@@ -59,7 +74,7 @@ const Welcome = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col flex-1 itens-center justify-start w-full md:mt-0 mt-10">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
@@ -77,6 +92,26 @@ const Welcome = () => {
                                 </p>
                             </div>
                         </div>
+                    </div>
+                    <div className="p-5 sm:w-95 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Address to..." name="addressTo" handleChange={() => {}}/>
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
+                        <Input placeholder="Address to..." handleChange={() => {}}/>
+                        <Input placeholder="Keyword (GIF)" name="keyword" handleChange={() => {}}/>
+                        <Input placeholder="Enter Message" name="message" handleChange={() => {}}/>
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+                            {true ? (
+                                <Loader/>
+                            ) : (
+
+                                <button type="submit" onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                                >
+                                    Send now
+                                </button>
+                        
+                            )
+                            }
                     </div>
                 </div>
 
